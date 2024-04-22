facbb15c98fbd18b6a0a989b5e4e486c1d5a8323
MarcosJhonata <marcosjhonata23@gmail.com>
2024-04-22 20:18:26 -0300

    Adicionado mensagem de erro , estilização no index ,  estilizaçao do componete user , e add funcionalidades em user e home

--- GithubFinder/src/routes/Home.tsx
+++ GithubFinder/src/routes/Home.tsx
@@ -2,15 +2,22 @@ import Search from "../components/Search";
 import { useState } from "react";
 import { UserProps } from "../Types/user";
 import User from "../components/User";
+import Error from "../components/Error";
 
 const Home = () => {
 
-    const [user, setUSer] = useState<UserProps | null>(null);
-
+    const [user, setUser] = useState<UserProps | null>(null);
+    const [error , setError] = useState(false)
     const loadUser= async(userName: string) => {
+        setError(false);
+        setUser(null);
 
         const res = await fetch(`https://api.github.com/users/${userName}`);
         const data = await res.json();
+        if (res.status === 404) {
+            setError(true);
+            return;
+        }
         const {avatar_url, login, location, followers, following} = data;
         const userData: UserProps = {
             avatar_url,
@@ -19,12 +26,13 @@ const Home = () => {
             followers,
             following,
         }
-        setUSer(userData);
+        setUser(userData);
     };
     return (
         <div>
             <Search loadUser={loadUser} />
             {user && <User{...user}/>}
+            {error && <Error/>}
         </div>
     )
 };
